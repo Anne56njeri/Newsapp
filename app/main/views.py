b0c370e669c3ec6736bffb13f850e70be366a354
MaryAnne45 <mary.jereh@gmail.com>
2018-04-07 21:30:55 +0300

    added a view function to display the business news sources

--- app/main/views.py
+++ app/main/views.py
@@ -2,3 +2,10 @@ from flask import render_template,request,url_for
 from . import main
 from .. requests import
 from ..models import Sources
+
+@main.route('/')
+def index():
+    business_source = get_sources('business')
+    print(business_source)
+    title = 'Home - welcome to Newsapp'
+    return render_template('index.html' title= title ,business = business_source)
