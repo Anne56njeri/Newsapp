5a12a0046c43f533debcf79ccc9f414ae160951b
MaryAnne45 <mary.jereh@gmail.com>
2018-04-08 22:47:46 +0300

    created an articles class t define the objects

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -4,4 +4,4 @@
 from flask import Blueprint
 # intializing the blueprint class
 main =Blueprint('main',__name__)
-from . import views
+from . import views,errors
--- app/models.py
+++ app/models.py
@@ -9,3 +9,17 @@ class Sources:
         self.category=category
         self.country=country
         self.description=description
+class Articles:
+    '''
+    an article class to define the article objects
+    '''
+    def __init__(self,id,name,author,date,title,url,image,):
+        self.id=id
+        self.name=name
+        self.author=author
+        self.date=date
+        self.author=author
+        self.date=date
+        self.title=title
+        self.url=url
+        self.image=image
